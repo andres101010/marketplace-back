8053a0ea9e1fce7b84fe6de0297a473df286308c
Kelthaz <davidkelthaz@gmail.com>
2023-03-03 20:28:42 -0500

    Se genera servicio de get para providers

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ var adminRouter = require('./routes/adminProviders');
 var login = require('./routes/login');
 
 var app = express();
+const mysql = require('mysql')
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/adminProviders.js
+++ routes/adminProviders.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql')
+
+
+const conectBD = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'marketplace'
+})
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.send('Hi providers');
+    const {user, password} = req.body
+    const values = [user, password]
+    const sql = 'select * from providers'
+
+    conectBD.query(sql, values,(err,result) => {
+        if(err) {
+            res.status(500).send(err)
+        } else {
+            if (result.length > 0) {
+                res.status(200).send(result)
+            } else {
+                res.status(400).send('No hay proveedores registrados.')
+            }
+        }
+    })
 });
 
 module.exports = router;
